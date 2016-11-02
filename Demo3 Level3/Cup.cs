2ce4e303427c4907436572ae6993669b7d04ce74
秦泽瑞 <qzr19970105@outlook.com>
2016-11-02 21:15:06 +0800

    添加项目文件。

---
+++ Demo3 Level3/Cup.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Level3
+{
+    public class Cup
+    {
+        static Cup()
+        {
+            bottom_area = Console.WindowWidth * Console.WindowHeight;
+        }//静态构造函数，将bottom_area的值改变为Console.WindowWidth和Console.WindowHeight的乘积(在创建第一个实例或引用任何静态成员之前，将自动调用静态构造函数来初始化类)
+        private double height = 20, weight = 20;//无函数为其赋值，赋初值为20
+        public Cup()
+        {
+            this.height = 20;
+            this.weight = 20;
+        }//不含参数的构造函数
+        public Cup(double Height):this()
+        {
+            this.height = Height;
+        }//含有一个参数的构造函数
+        private static readonly double bottom_area;//将bottom_area声明成静态只读字段
+        public int count = 0;
+        public int leak;//新的int类型的字段 leak ，表示杯子的漏洞数
+        public void GetPrimeFactors(int num)
+        {
+            int i, j;
+            for (i = 2; i <= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    j = num / i;
+                    if (j == 1)
+                    {
+                        Console.Write(i);
+                        return;
+                    }
+                    else
+                    {
+                        Console.Write("{0}*", i);
+                        GetPrimeFactors(j);
+                        break;
+                    }
+                }
+            }
+        }//分解杯子实例容积的质因数
+        public void GetTimesAtEmpty(int capacity, int leak)
+        {
+            int remaining = capacity;
+            if (capacity < 136)
+            {
+                return;
+            }
+            if (remaining > 0)
+            {
+                count = count + remaining / 136;
+                GetTimesAtEmpty(capacity / 3, leak);
+            }
+        }//计算倒水的次数
+        public double WaterVolume
+        {
+            get
+            {
+                return leak * 7;
+            }
+            set
+            {
+                leak = (int)value / 7;
+            }
+        }//double类型的属性WaterVolume，表示杯子中水的体积。为其赋值时，将该值除以7后赋值给leak。该属性的返回值为leak乘以7的结果
+        public double Capacity
+        {
+            get
+            {
+                return height * bottom_area;
+            }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                if (weight > 10)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ Demo3 Level3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo3_Level3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Level3 (1)
+            Cup cup4 = new Cup();
+            int capacity = (int)cup4.Capacity;
+            Console.WriteLine("杯子容积为:{0}", (int)cup4.Capacity);//double强制转换为int
+            Console.Write("{0}=", capacity);
+            cup4.GetPrimeFactors(capacity);
+            Console.WriteLine();
+            #endregion
+
+            #region Level3 (2)
+            Cup cup5 = new Cup() { WaterVolume = 56 };
+            Console.WriteLine("杯子有{0}个漏洞",cup5.leak);
+            cup5.GetTimesAtEmpty((int)cup5.Capacity, (int)cup5.leak);
+            Console.WriteLine("总的倒水次数为{0}",cup5.count);
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
